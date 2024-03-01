bd879b10ab63fc6d75f921d6023b75d757dc51ed
Andrew Henderson <102596227+BobTheMartyr@users.noreply.github.com>
2024-03-01 18:12:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,84 @@
 let price = 19.5
 
-let cid = [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]
+let cid = [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
 
 
 const cash = document.getElementById('cash')
 const purchaseBtn = document.getElementById('purchase-btn')
-const priceContainer = document.getElementById('price-left')
-const registerTotalContainer = document.getElementById('change-title')
-const changeInDrawer = document.getElementById('change-in-drawer')
+const priceDisplay = document.getElementById('price-left')
+const registerTotalDisplay = document.getElementById('change-title')
+const displayCid = document.getElementById('change-in-drawer')
 const status = document.getElementById('status-title')
-const changeDueContainer = document.getElementById('change-due')
+const displayChangeDue = document.getElementById('change-due')
 
-priceContainer.innerHTML = `<p>$${price}</p>`
 
-let registerTotal = parseFloat(cid.map(el => el[1]).reduce((t,c) => t + c).toFixed(2))
-
-const updateRegisterTotal = () => {
-  registerTotal = parseFloat(cid.map(el => el[1]).reduce((t,c) => t + c).toFixed(2))
-  registerTotalContainer.innerHTML = `<p>Register Total: $${registerTotal}</p>`
-}
-
-const updateRegister = () => {
-  changeInDrawer.innerHTML = ''
-  cid.forEach(el => {
-    changeInDrawer.innerHTML += `<div id='money'<p>${el[0]}</p><p class='dollar-amt'>$${parseFloat(el[1].toFixed(2))}</p></div>`
-  })
-}
 
 const formatResults = (status, change) => {
-  changeDueContainer.innerHTML = ''
-  changeDueContainer.innerHTML += `<p class='dollar-amt'>Status: ${status}</p>`
+  displayChangeDue.innerHTML = ''
+  displayChangeDue.innerHTML = `<p>Status: ${status}</p>`
   change.forEach(el => {
-    changeDueContainer.innerHTML += `<p class='dollar-amt'>${el[0]}: $${el[1]}</p>`
+    displayChangeDue.innerHTML += `<p>${el[0]}: $${el[1]}</p>`
   })
 }
 
 
-window.addEventListener('load', () => {
-  updateRegisterTotal()
-  updateRegister()
-}) 
-
-purchaseBtn.addEventListener('click', () => {
-  changeDueContainer.innerHTML = ''
-  if( Number(cash.value) < price ) {
+const checkRegister = () => {
+  let payment = Number(cash.value)
+  
+  if( payment < price ){
     alert('Customer does not have enough money to purchase the item')
     cash.value = ''
     return
-    }
-  if( Number(cash.value) === price) {
-      changeDueContainer.innerHTML +=  `<p class='dollar-amt' style='text-align:center;font-size: 30px;'>No change due - customer paid with exact cash</p>`
-      cash.value = ''
-      return
-    }
-  const result = checkRegister(Number(cash.value) - price)
-
-  formatResults(result.status, result.change)
-  updateRegisterTotal()
-  updateRegister()
-})
-
-
-const checkRegister = (changeDue) => {
+  }
+  if( payment === price ){
+    displayChangeDue.innerHTML += `<p>No change due - customer paid with exact cash</p>`
+    cash.value = ''
+    return
+  }
 
   let result = {
-    status: 'pending',
+    status: 'OPEN',
     change: []
-    }
-  
+  }
 
-  if( changeDue > registerTotal ){
-    result.status = 'INSUFFICIENT_FUNDS'
-    return result
-  } else if( changeDue === registerTotal ){
+  let changeDue = parseFloat((payment - price).toFixed(2))
+  let regTotal = parseFloat(cid.map(el => el[1]).reduce((t,c) => t + c).toFixed(2))
+  let denoms = [100, 20, 10, 5, 1, .25, .1, .05, .01]
+  let revCid = [...cid].reverse()
+
+  if( changeDue === regTotal ){
     result.status = 'CLOSED'
   }
 
-  let denoms = [100, 20, 10, 5, 1, .25, .1, .05, .01]
-  cid.reverse()
-
+  if( changeDue > regTotal ){
+    displayChangeDue.innerHTML = `<p>Status: INSUFFICIENT_FUNDS</p>`
+    return
+  }
+  
 
   for( let i = 0; i < denoms.length; i++ ){
-    if( denoms[i] <= changeDue && cid[i][1] > 0 ){
+    if( denoms[i] <= changeDue && revCid[i][1] > 0 ){
       let total = 0
-      let cashType = cid[i][0]
-      while( denoms[i] <= changeDue && cid[i][1] > 0 ){
-        total += parseFloat(denoms[i].toFixed(2))
-        changeDue -= parseFloat(denoms[i].toFixed(2))
-        cid[i][1] -= parseFloat(denoms[i].toFixed(2))
-        changeDue = parseFloat(changeDue.toFixed(2))
-        total = parseFloat(total.toFixed(2))
+      let cashType = revCid[i][0]
+      while( denoms[i] <= changeDue && revCid[i][1] > 0 ){
+        total++
+        changeDue = parseFloat((changeDue -= denoms[i]).toFixed(2))
+        revCid[i][1] -= denoms[i]
       }
-      result.change.push([cashType, total])
+      result.change.push([cashType, total * denoms[i]])
     }
   }
-  
-  
+
   if( changeDue > 0 ){
-    result.status = 'INSUFFICIENT_FUNDS'
-  } else if( result.status === 'pending' ) {
-    result.status = 'OPEN'
+    displayChangeDue.innerHTML = `<p>Status: INSUFFICIENT_FUNDS</p>`
+    cash.value = ''
+    return
   }
 
-  cid.reverse()
   return result
 }
 
+purchaseBtn.addEventListener('click', () => {
+  const result = checkRegister()
+  formatResults(result.status, result.change)
+})
